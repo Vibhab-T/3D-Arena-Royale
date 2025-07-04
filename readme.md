473228eab755aa35b8e78b8b82db25b09fbb01aa
Vibhab-T <vibhabtimsina86@gmail.com>
2025-07-04 06:39:04 +0545

    readme

--- readme.md
+++ readme.md
@@ -1,3 +1,9 @@
 A 3D FPS, Arena Survival Game.
 
 Kill the bats. That's really it.
+
+image("example-1")
+image("example-2")
+image("example-3")
+
+<img src="example 2">
--- scripts/game.gd
+++ scripts/game.gd
@@ -1,11 +1,24 @@
 extends Node3D
-@onready var score_label: Label = $Label
+@onready var timer: Timer = $Timer
+@onready var score_label: Label = $ScoreLabel
+@onready var time_label: Label = $TimeLabel
+
 
 var player_score = 0
+var time_left = 10
+var timer_started = false
+
+func _physics_process(delta: float) -> void:
+	if(time_left > 0 and timer_started):
+		time_left -= 1 * delta 
+		time_label.text = "Time left: " + str(int(time_left)) + "s"
 
 func increase_score():
 	player_score += 1
 	score_label.text = "Score: " + str(player_score)
+	if (player_score == 1):
+		timer.start()
+		timer_started = true
 
 
 func _on_spawner_mob_spawned(mob: Variant) -> void:
@@ -13,4 +26,12 @@ func _on_spawner_mob_spawned(mob: Variant) -> void:
 
 
 func _on_kill_plane_body_entered(body: Node3D) -> void:
+	get_tree().reload_current_scene.call_deferred()
+
+
+func _on_timer_timeout() -> void:
+	time_label.text = "Time Over. Reloading scene"
+	
+	Engine.set_time_scale(0.25)
+	
 	get_tree().reload_current_scene.call_deferred()
